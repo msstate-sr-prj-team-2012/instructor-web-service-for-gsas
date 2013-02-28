6c5137fc1b72989033ebca03061de071550a0010
Chad McDaniel <ccm290@msstate.edu>
2013-02-28 14:02:32 -0600

    another change

--- html/content/js/onPar_admin_functions.js
+++ html/content/js/onPar_admin_functions.js
@@ -20,8 +20,8 @@ function getUserData(){
     userData = [];
     for(var i = 0; i < 10; i++){
         userData.push({
-                userName: "Chad McDaniel",
-                userEmail: "ccm290@msstate.edu"
+                userName: 'Chad McDaniel',
+                userEmail: 'ccm290@msstate.edu'
             });
     }
 	
@@ -35,8 +35,8 @@ function getUserData(){
             data: userData,
             colNames:['User Name', 'Email'],
             colModel:[
-                {name:'User Name', index:'userName', width: 140, sorttype: 'text', align:'center'},
-                {name:'Email', index:'userEmail', width:140, align:'center', sorttype:'text', align:'center'},
+                {name:'User Name', index:'userName', width: 140, align:'center'},
+                {name:'Email', index:'userEmail', width:140, align:'center', align:'center'},
             ],
             rowNum: 5,
             rowList:[3,5,10],
