3159e54899e9153b77e17ce0f08979cbd221d122
Kevin Benton <krbenton@windstream.net>
2013-03-05 14:05:54 -0600

    local testing

--- localTesting/html/content/js/bar_graph.js
+++ localTesting/html/content/js/bar_graph.js
@@ -1,6 +1,19 @@
-//get all rounds
+// github copy
+/****************************************************************************
+ *
+ * Global Variables
+ *
+ ****************************************************************************/
+
+var currentView = 'v1';
+var currentHole = 1;   
+var currentRound = '1';
+var hole;
 var rounds = localStorage.getObject('rounds');
-    
+var html='';
+var graphView = 'v1';
+var mapRound = localStorage.getObject('rounds')[0].rid;
+
 $(document).ready(function() 
 {
     $(".view_tabs li").click(function() 
@@ -14,13 +27,13 @@ $(document).ready(function()
     });
     var tags = new Array("shot 1", "shot 2", "shot 3", "shot 4","hot 5")
     var values = new Array("55", "80", "95", "32","900")
-    basicbargraph(tags,values);
+    //basicbargraph(tags,values);
+    graph2(tags,values);
     document.getElementById(mapRound).className += ' selected_tab';
 });
         
  function basicbargraph(tags, value) {
     var length = 200;
-    var html;
 
     for (i = 0; i < tags.length; i++) {
         html+= '<div style="font-size: 10pt; color: #AAAAAA;">' + tags[i] + ': </div><div style="font-size:0 1pt; color: #AAAAAA;"><img src="pixel.jpg" width="' + value[i] / 100 * length + '" height="15" border="1" style="border-color: #00AA00"> ' + value[i] + 'Yards</div>';
@@ -47,9 +60,6 @@ function graph2(tags, value) {
     html+= ' </p>';
     document.getElementById('map_image').innerHTML = html;
 }
-      
-var graphView = 'v1';
-var mapRound = localStorage.getObject('rounds')[0].rid;
 
 function changeView(view)
 {
@@ -148,4 +158,49 @@ function convertGPStoYardslong(lat1, long1, lat2, long2)
     var dlonggps = 13950131.0*Math.asin(Math.sqrt(Math.cos(lat2) * Math.cos(lat1))*Math.pow( Math.sin( ( long1 - long2)/2)),2);
     
     return dlonggps;
+}
+function getDistance(startLat, startLon, endLat, endLon){
+    var dLat = (endLat - startLat) * (Math.PI / 180.0);
+    var dLon = (endLon - startLon) * (Math.PI / 180.0);
+    var lat1 = startLat * (Math.PI / 180.0);
+    var lat2 = endLat * (Math.PI / 180.0);
+//    var a = Math.pow(Math.sin(dLat/2),2) + Math.cos(startLat) * Math.cos(endLat) * Math.pow(Math.sin(dLon/2),2);
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return (EARTH_RADIUS_IN_YARDS * c).toFixed(2);
+}
+function getShotDatabg(){
+    
+    // filters round object for currentHole
+    hole = (rounds[currentRound].holes).filter(function(obj) { return (obj.holeNumber == currentHole) })[0];
+    
+    shotData = [];
+    if(hole === undefined || hole.shots.length === 0){
+        createShotGrid();
+        $('#shots').setCaption("Shots (Hole " + currentHole + ") <span style='color:#ff0000' > -- No Data Found -- </span>").trigger("reloadGrid");
+    }
+    else{
+        for(var i = 0;i < hole.shots.length; i++){
+            var startLat = hole.shots[i].startLatitude;
+            var startLong = hole.shots[i].startLongitude;
+            var aimLat = hole.shots[i].aimLatitude;
+            var aimLong = hole.shots[i].aimLongitude;
+            var endLat = hole.shots[i].endLatitude;
+            var endLong = hole.shots[i].endLongitude;
+
+
+            // gets distance
+            var distance = getDistance(startLat, startLong, endLat, endLong);
+
+            // gets club name
+            var club = getClubName(hole.shots[i].club);
+
+            shotData.push({
+                shot: hole.shots[i].shotNumber,
+                club: club,
+                distance: distance,
+            })
+        }  
+    }
 }
