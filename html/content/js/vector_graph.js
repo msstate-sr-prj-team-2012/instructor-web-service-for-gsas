ee6b6b3dfc1b2e99c308ece874080578c9bba8bd
Craig Ellis <wce37@msstate.edu>
2013-03-21 14:02:04 -0500

    Vector Graph Fix

--- html/content/js/vector_graph.js
+++ html/content/js/vector_graph.js
@@ -10,6 +10,8 @@ var rounds = localStorage.getObject('rounds');
 var inc = 1;
 var hole;
 var Shots = 0;
+//var club = 1;
+
 /****************************************************************************
  *
  * Degree/Radian conversion functions
@@ -19,10 +21,14 @@ function drawLine()
 {
 
     var shotArray = [];
+    var color;
+   // var clubName = getClubName(club);
+
+
     html = "<svg>\n";
 
     html += "<ellipse cx= '425' cy= '350'  rx= '425' ry='325' stroke= 'black'\n ";
-    html += "stroke-width= '2' fill= 'lightblue'>\n";
+    html += "stroke-width= '2' fill= '#F4F4F4'>\n";
     html += "</ellipse>\n\n";
 
     html += "<line x1 = '425'  y1= '25' x2= '425' y2= '350' \n ";
@@ -62,9 +68,12 @@ function drawLine()
     {
         for (var j = 0; j < shotArray.length; j++)
         {
-
+            var club = shotArray[j].club;
+            var color = 'white';
             var startLat = shotArray[j].startLatitude;
             var startLon = shotArray[j].startLongitude;
+            var clubX;
+            var clubY;
 
 
             var aimLon = shotArray[j].aimLongitude;
@@ -78,17 +87,19 @@ function drawLine()
             var E = ((endLat * endLon) ^ 2) ^ (1 / 2);
             var lowerAE = A * E;
             var angle = Math.acos(upperAE / lowerAE);
-            
+
             var radEndLat = (endLat * Math.PI) / 180;
             var radEndLon = (endLon * Math.PI) / 180;
             var pxEndLat = 5376 * Math.tan(radEndLat / 2);
             var pxEndLon = 5376 * Math.tan(radEndLon / 2);
-            
+
             var radian = (angle * Math.PI) / 180;
             var px = 5376 * Math.tan(radian / 2);
 
-            var slice = px +425;
+            var slice = px + 425;
             var hash = px - 90;
+
+            //Draw the line
             if (startLon <= aimLon && startLat < aimLat)//Start South : Aim/End North
             {
                 if (aimLon > endLon)
@@ -97,9 +108,12 @@ function drawLine()
                     html += "stroke='black' stroke-width='2'>\n";
                     html += "<title> " + px + "</title>\n";
                     html += "</line>\n\n";
-                    html += "<circle cx= '"+pxEndLon+"' cy= '"+pxEndLat+"' r= '3' stroke= 'black'\n ";
+                    html += "<circle cx= '" + pxEndLon + "' cy= '" + pxEndLat + "' r= '3' stroke= 'black'\n ";
                     html += "stroke-width= '2' fill= 'black'>\n";
                     html += "</circle>\n\n";
+                    
+                    clubX=px;
+                    clubY=px;
                 }
                 if (aimLon <= endLon)
                 {
@@ -107,9 +121,12 @@ function drawLine()
                     html += "stroke='black' stroke-width='2'>\n";
                     html += "<title> " + px + "</title>\n";
                     html += "</line>\n\n";
-                    html += "<circle cx= '"+pxEndLon+"' cy= '"+pxEndLat+"' r= '3' stroke= 'black'\n ";
+                    html += "<circle cx= '" + pxEndLon + "' cy= '" + pxEndLat + "' r= '3' stroke= 'black'\n ";
                     html += "stroke-width= '2' fill= 'black'>\n";
                     html += "</circle>\n\n";
+                    
+                    clubX=slice;
+                    clubY=px;
                 }
             }
             if (startLon >= aimLon && startLat > aimLat)//Start North : Aim/End South
@@ -120,9 +137,12 @@ function drawLine()
                     html += "stroke='black' stroke-width='2'>\n";
                     html += "<title> " + px + "</title>\n";
                     html += "</line>\n\n";
-                    html += "<circle cx= '"+pxEndLon+"' cy= '"+pxEndLat+"' r= '3' stroke= 'black'\n ";
+                    html += "<circle cx= '" + pxEndLon + "' cy= '" + pxEndLat + "' r= '3' stroke= 'black'\n ";
                     html += "stroke-width= '2' fill= 'black'>\n";
                     html += "</circle>\n\n";
+                    
+                    clubX=slice;
+                    clubY=px;
                 }
                 if (aimLon <= endLon)
                 {
@@ -130,14 +150,207 @@ function drawLine()
                     html += "stroke='black' stroke-width='2'>\n";
                     html += "<title> " + px + "</title>\n";
                     html += "</line>\n\n";
-                    html += "<circle cx= '"+pxEndLon+"' cy= '"+pxEndLat+"' r= '3' stroke= 'black'\n ";
+                    html += "<circle cx= '" + pxEndLon + "' cy= '" + pxEndLat + "' r= '3' stroke= 'black'\n ";
                     html += "stroke-width= '2' fill= 'black'>\n";
                     html += "</circle>\n\n";
+                    
+                    clubX=px;
+                    clubY=px;
                 }
             }
             //  var startToAimDistance = Math.sqrt(((startLon - aimLon) ^ 2) + ((startLat - aimLat) ^ 2));
             //  var startToEndDistance = Math.sqrt(((startLon - endLon) ^ 2) + ((startLat - endLat) ^ 2));
-        }
+
+            var distance;
+
+            // draw line between two points
+            //  var html = "<line x1='" + X1 + "' y1='" + Y1 + "' x2='" + X2 + "' y2='" + Y2 + "' stroke='black' stroke-width='2' />\n";
+
+            // draw triangle with the appropriate color
+            if (currentClub == '0')
+            {
+                if (club >= 1 && club <= 6)
+                {
+                    if (club === 1)
+                        color = 'red';
+                    else if (club === 2)
+                        color = 'green';
+                    else if (club === 3)
+                        color = 'blue';
+                    else if (club === 4)
+                        color = 'orange';
+                    else if (club === 5)
+                        color = 'purple';
+                    else if (club === 6)
+                        color = 'lightblue';
+
+                    // points='topX,topY rightX,rightY leftX,leftY'  
+                    // drawn centered on the point with a 6px difference in each direction
+                    html += "<polygon points='" + clubX  + "," + (clubY - 6) + " " + (clubX  + 6) + "," + (clubY + 6) + " " + (clubX  - 6) + "," + (clubY + 6) + "' stroke='" + color + "' stroke-width='2' fill='white' >\n" +
+                            "<title> </title>\n" +
+                            "</polygon>\n";
+
+                }
+
+                // draw square with the appropriate color
+                else if (club >= 7 && club <= 9)
+                {
+                    if (club === 7)
+                        color = 'red';
+                    else if (club === 8)
+                        color = 'green';
+                    else if (club === 9)
+                        color = 'blue';
+                    else if (club === 10)
+                        color = 'orange';
+                    else if (club === 11)
+                        color = 'purple';
+
+                    html += "<rect x='" + clubX  + "' y='" + clubY + "' width='10' height='10' stroke='" + color + "' stroke-width='2' fill='white' >\n" +
+                            "<title> </title>\n" +
+                            "</rect>\n";
+                }
+
+                // draw circle with the appropriate color
+                else if (club >= 12 && club <= 19)
+                {
+                    if (club === 12)
+                        color = 'red';
+                    else if (club === 13)
+                        color = 'green';
+                    else if (club === 14)
+                        color = 'blue';
+                    else if (club === 15)
+                        color = 'orange';
+                    else if (club === 16)
+                        color = 'purple';
+                    else if (club === 17)
+                        color = 'lightblue';
+                    else if (club === 18)
+                        color = 'pink';
+                    else if (club === 19)
+                        color = 'brown';
+
+                    html += "<circle cx='" + clubX  + "' cy='" + clubY + "' r='5' stroke='" + color + "' stroke-width='2' fill='white' >" +
+                            "<title></title>\n" +
+                            "</circle>\n";
+
+                }
+
+                // draw diamond with the approriate color
+                else if (club >= 20 && club <= 24)
+                {
+                    if (club === 20)
+                        color = 'red';
+                    else if (club === 21)
+                        color = 'green';
+                    else if (club === 22)
+                        color = 'blue';
+                    else if (club === 23)
+                        color = 'orange';
+                    else if (club === 24)
+                        color = 'purple';
+
+                    // points='topX,topY rightX,rightY bottomX,bottomY leftX,leftY'
+                    // diamond centered on point with 6px difference in each direction
+                    html += "<polygon points='" + clubX  + "," + (clubY - 6) + " " + (clubX  + 6) + "," + clubY + " " + clubX + "," + (clubY + 6) + " " + (clubX  - 6) + "," + clubY + "' stroke='" + color + "' stroke-width='2' fill='white' >\n" +
+                            "<title></title>\n" +
+                            "</polygon>\n";
+
+                }
+            }
+            else if (currentClub == '1')
+            {
+                    if (club === 1)
+                        color = 'red';
+                    else if (club === 2)
+                        color = 'green';
+                    else if (club === 3)
+                        color = 'blue';
+                    else if (club === 4)
+                        color = 'orange';
+                    else if (club === 5)
+                        color = 'purple';
+                    else if (club === 6)
+                        color = 'lightblue';
+
+                    // points='topX,topY rightX,rightY leftX,leftY'  
+                    // drawn centered on the point with a 6px difference in each direction
+                    html += "<polygon points='" + clubX  + "," + (clubY - 6) + " " + (clubX + 6) + "," + (clubY + 6) + " " + (clubX  - 6) + "," + (clubY + 6) + "' stroke='" + color + "' stroke-width='2' fill='white' >\n" +
+                            "<title></title>\n" +
+                            "</polygon>\n";
+
+                  
+            }
+            else if (currentClub == '2')
+            {
+
+                // draw square with the appropriate color
+                /* else if (club >= 7 && club <= 9)
+                 {*/
+                if (club === 7)
+                    color = 'red';
+                else if (club === 8)
+                    color = 'green';
+                else if (club === 9)
+                    color = 'blue';
+                else if (club === 10)
+                    color = 'orange';
+                else if (club === 11)
+                    color = 'purple';
+
+                html += "<rect x='" + clubX  + "' y='" + clubY + "' width='10' height='10' stroke='" + color + "' stroke-width='2' fill='white' >\n" +
+                        "<title> </title>\n" +
+                        "</rect>\n";
+            }
+
+           
+            else if (currentClub == '3')
+            {
+                if (club === 12)
+                    color = 'red';
+                else if (club === 13)
+                    color = 'green';
+                else if (club === 14)
+                    color = 'blue';
+                else if (club === 15)
+                    color = 'orange';
+                else if (club === 16)
+                    color = 'purple';
+                else if (club === 17)
+                    color = 'lightblue';
+                else if (club === 18)
+                    color = 'pink';
+                else if (club === 19)
+                    color = 'brown';
+
+                html += "<circle cx='" + clubX + "' cy='" + clubY + "' r='5' stroke='" + color + "' stroke-width='2' fill='white' >" +
+                        "<title></title>\n" +
+                        "</circle>\n";
+
+            }
+            else if(currentClub == '4')
+                {
+                    if (club === 20)
+                        color = 'red';
+                    else if (club === 21)
+                        color = 'green';
+                    else if (club === 22)
+                        color = 'blue';
+                    else if (club === 23)
+                        color = 'orange';
+                    else if (club === 24)
+                        color = 'purple';
+
+                    // points='topX,topY rightX,rightY bottomX,bottomY leftX,leftY'
+                    // diamond centered on point with 6px difference in each direction
+                    html += "<polygon points='" + clubX + "," + (clubY - 6) + " " + (clubX  + 6) + "," + clubY + " " + clubX  + "," + (clubY + 6) + " " + (clubX  - 6) + "," + clubY + "' stroke='" + color + "' stroke-width='2' fill='white' >\n" +
+                            "<title></title>\n" +
+                            "</polygon>\n";
+
+                
+                }
+        } // end draw shape
     }
 
     html += "</svg>\n";
